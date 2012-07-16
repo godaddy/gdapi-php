464bd6b49ac0a1149ca026e4276dec20263e3a30
Vincent Fiduccia <vfiduccia@godaddy.com>
2012-07-16 13:52:13 -0700

    Add license to each file~

--- class/CustomException.php
+++ class/CustomException.php
@@ -1,4 +1,26 @@
 <?php
+/*
+ * Copyright (c) 2012 Go Daddy Operating Company, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 namespace GDAPI;
 
 # From http://us2.php.net/manual/en/language.exceptions.php#91159
@@ -23,7 +45,7 @@ abstract class CustomException extends \Exception implements ExceptionInterface
 {
   protected $message = 'Unknown exception';     // Exception message
   private   $string;                            // Unknown
-  protected $code    = 0;                       // User-defined exception code
+  protected $code = 0;                          // User-defined exception code
   protected $file;                              // Source filename of exception
   protected $line;                              // Source line of exception
   private   $trace;                             // Unknown
@@ -39,4 +61,5 @@ abstract class CustomException extends \Exception implements ExceptionInterface
                               . "{$this->getTraceAsString()}";
   }
 }
+
 ?>
--- init.php
+++ init.php
@@ -1,17 +1,39 @@
 <?php
+/*
+ * Copyright (c) 2012 Go Daddy Operating Company, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
-require_once('CustomException.php');
-require_once('APIException.php');
 
-require_once('RequestInterface.php');
-require_once('CurlRequest.php');
-require_once('CacheInterface.php');
+require_once('class/CustomException.php');
+require_once('class/APIException.php');
 
-require_once('Resource.php');
-require_once('Error.php');
-require_once('Collection.php');
-require_once('Type.php');
+require_once('class/RequestInterface.php');
+require_once('class/CurlRequest.php');
+require_once('class/CacheInterface.php');
 
-require_once('Client.php');
+require_once('class/Resource.php');
+require_once('class/Error.php');
+require_once('class/Collection.php');
+require_once('class/Type.php');
+
+require_once('class/Client.php');
 
 ?>
